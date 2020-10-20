3ed860b717706eaabd53af5a6e5109275f7ff2a8
Jose Guadalupe Cortes Cruz <jose17y10@gmail.com>
2020-10-20 17:57:52 -0500

    Proyecto terminado

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pelisapp'`, () => {
+  it(`should have as title 'peliculasapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pelisapp');
+    expect(app.title).toEqual('peliculasapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pelisapp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('peliculasapp app is running!');
   });
 });
